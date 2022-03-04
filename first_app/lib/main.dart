7013ae0f4f3f46de4971c5e4bf1594e41e002d07
Naoto-110616 <siodeitadaku@gmail.com>
2022-03-04 21:55:01 +0900

    Building a Widget tree

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -4,10 +4,17 @@ import "package:flutter/material.dart";
 //   runApp(MyApp());
 // }
 
-void main()=>runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Hello!"),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my default text!'),
+      ),
+    );
   }
 }
